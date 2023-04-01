1d9628309c3386fdf2e3c5311253e353096a04b1
Bin Yang <bin.yang@algotune.com>
2023-03-31 21:37:19 -0400

    minor format changes

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -4,11 +4,13 @@ export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
-        <a
+          <img className="ml-2 w-10 h-10 rounded-full mx-auto" src="/wb_head.jpg" alt="" width="10" height="10">
+          </img>
+          <a
           className="ml-2 hover:opacity-50"
-          href="https://code-scaffold.vercel.app"
+          href="https://openai-ui-taupe.vercel.app/"
         >
-          Chatbot UI
+          Warren Buffett Is Here
         </a>
       </div>
     </div>
