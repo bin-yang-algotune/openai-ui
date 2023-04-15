5bfbeb86e1247a81ede6dd2a8a15c8b7a23bf798
Bin Yang <bin.yang@algotune.com>
2023-04-14 21:00:43 -0400

    add href for weblink

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -5,6 +5,15 @@ interface Props {
   message: Message;
 }
 
+function urlify(text: string) {
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '">' + url + '</a>';
+    })
+    // or alternatively
+    // return text.replace(urlRegex, '<a href="$1">$1</a>')
+}
+
 export const ChatMessage: FC<Props> = ({ message }) => {
   return (
     <div className={`flex flex-col ${message.role === "assistant" ? "items-start" : "items-end"}`}>
@@ -12,7 +21,7 @@ export const ChatMessage: FC<Props> = ({ message }) => {
         className={`flex items-center ${message.role === "assistant" ? "bg-neutral-200 text-neutral-900" : "bg-blue-500 text-white"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
         style={{ overflowWrap: "anywhere" }}
       >
-        {message.content}
+        {urlify(message.content)}
       </div>
     </div>
   );
